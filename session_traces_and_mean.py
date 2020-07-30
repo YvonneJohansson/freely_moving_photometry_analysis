a6f126077200e1b16874909d811b616b2a381aac
fgreenstreet <32090651+fgreenstreet@users.noreply.github.com>
2020-07-30 11:27:59 +0100

    made it easier to run post processing per session or per mouse

--- session_traces_and_mean.py
+++ session_traces_and_mean.py
@@ -7,21 +7,41 @@ import pickle
 from utils.individual_trial_analysis_utils import SessionData
 import pandas as pd
 
+def get_all_experimental_records():
+    experiment_record = pd.read_csv('W:\\photometry_2AC\\experimental_record.csv')
+    experiment_record['date'] = experiment_record['date'].astype(str)
+    return experiment_record
 
-experiment_record = pd.read_csv('W:\\photometry_2AC\\experimental_record.csv')
-experiment_record['date'] = experiment_record['date'].astype(str)
 
-for index, experiment in experiment_record.iterrows():
-    saving_folder = 'W:\\photometry_2AC\\processed_data\\' + experiment['mouse_id'] + '\\'
-    restructured_data_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'restructured_data.pkl'
-    trial_data = pd.read_pickle(saving_folder + restructured_data_filename)
-    dff_trace_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'smoothed_signal.npy'
-    dff = np.load(saving_folder + dff_trace_filename)
+def add_experiment_to_aligned_data(experiments_to_add):
+    for index, experiment in experiments_to_add.iterrows():
+        saving_folder = 'W:\\photometry_2AC\\processed_data\\' + experiment['mouse_id'] + '\\'
+        restructured_data_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'restructured_data.pkl'
+        trial_data = pd.read_pickle(saving_folder + restructured_data_filename)
+        dff_trace_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'smoothed_signal.npy'
+        dff = np.load(saving_folder + dff_trace_filename)
 
-    session_traces = SessionData(experiment['fiber_side'], experiment['recording_site'], experiment['mouse_id'], experiment['date'])
-    session_traces.get_choice_responses()
-    session_traces.get_cue_responses()
-    session_traces.get_reward_responses()
-    aligned_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'aligned_traces.p'
-    save_filename = saving_folder + aligned_filename
-    pickle.dump(session_traces, open(save_filename, "wb"))
+        session_traces = SessionData(experiment['fiber_side'], experiment['recording_site'], experiment['mouse_id'], experiment['date'])
+        session_traces.get_choice_responses()
+        session_traces.get_cue_responses()
+        session_traces.get_reward_responses()
+        aligned_filename = experiment['mouse_id'] + '_' + experiment['date'] + '_' + 'aligned_traces.p'
+        save_filename = saving_folder + aligned_filename
+        pickle.dump(session_traces, open(save_filename, "wb"))
+
+
+if __name__ == '__main__':
+    mouse_id = 'SNL_photo19'
+    date = 'all'
+    all_experiments = get_all_experimental_records()
+
+    if mouse_id =='all' & date == 'all':
+        experiments_to_process = all_experiments
+    elif mouse_id == 'all' & date!= 'all':
+        experiments_to_process = all_experiments[all_experiments['date'] == date]
+    elif mouse_id != 'all' & date == 'all':
+        experiments_to_process = all_experiments[all_experiments['mouse_id'] == mouse_id]
+    elif mouse_id != 'all' & date != 'all':
+        experiments_to_process = all_experiments[(all_experiments['date'] == date) & (all_experiments['mouse_id'] == mouse_id)]
+
+
